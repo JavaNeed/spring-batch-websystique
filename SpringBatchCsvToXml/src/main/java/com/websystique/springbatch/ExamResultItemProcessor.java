2c0b05740807528dafe25c0b1a0756220442d205
prateek <prateek.ashtikar512@gmail.com>
2016-04-01 10:24:00 +0530

    Initial commit - spring-batch-read-a-csv-file-and-write-to-an-xml-file

---
+++ SpringBatchCsvToXml/src/main/java/com/websystique/springbatch/ExamResultItemProcessor.java
@@ -0,0 +1,27 @@
+package com.websystique.springbatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+import com.websystique.springbatch.model.ExamResult;
+
+public class ExamResultItemProcessor implements ItemProcessor<ExamResult, ExamResult>{
+	private static final Logger LOGGER = LoggerFactory.getLogger(ExamResultItemProcessor.class);
+	
+	@Override
+	public ExamResult process(ExamResult result) throws Exception {
+		LOGGER.debug("Processing result :"+result);
+		
+		/*
+		 * Only return results which are more than 60%
+		 * 
+		 */
+		if(result.getPercentage() < 60){
+			return null;
+		}
+		
+		return result;
+	}
+
+}
---
+++ SpringBatchCsvToXml/src/main/java/com/websystique/springbatch/ExamResultJobListener.java
@@ -0,0 +1,47 @@
+package com.websystique.springbatch;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+
+public class ExamResultJobListener implements JobExecutionListener{
+	private static final Logger LOGGER = LoggerFactory.getLogger(ExamResultJobListener.class);
+	private DateTime startTime, stopTime;
+	
+	@Override
+	public void beforeJob(JobExecution jobExecution) {
+		startTime = new DateTime();
+		LOGGER.debug("-----------------------------------------");
+		LOGGER.debug("ExamResult Job starts at :"+startTime);
+	}
+	
+
+	@Override
+	public void afterJob(JobExecution jobExecution) {
+		stopTime = new DateTime();
+		LOGGER.debug("ExamResult Job stops at :"+stopTime);
+		LOGGER.debug("Total time take in millis :"+getTimeInMillis(startTime , stopTime));
+		LOGGER.debug("------------------------------------------------");
+		
+		if(jobExecution.getStatus() == BatchStatus.COMPLETED){
+			LOGGER.debug("ExamResult job completed successfully");
+			//Here you can perform some other business logic like cleanup
+		}else if(jobExecution.getStatus() == BatchStatus.FAILED){
+			LOGGER.debug("ExamResult job failed with following exceptions ");
+			List<Throwable> exceptionList = jobExecution.getAllFailureExceptions();
+			for(Throwable th : exceptionList){
+				System.err.println("exception :" +th.getLocalizedMessage());
+			}
+		}
+	}
+	
+	private long getTimeInMillis(DateTime start, DateTime stop){
+		return stop.getMillis() - start.getMillis();
+	}
+	
+}
